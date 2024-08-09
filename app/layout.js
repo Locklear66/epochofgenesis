14e15f5f7310753942bf238eda48190c3a6453fd
Lance <lance@Lances-MacBook-Pro.local>
2024-08-08 20:36:42 -0500

    added a hamburger menu

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./nav/page"
 import Footer from "./footer/page"
+import HamburgerMenu from "./hamburger/page"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <HamburgerMenu /> 
         <Navbar />
         {children}
         <Footer />
