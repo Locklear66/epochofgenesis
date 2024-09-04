546efb831f1bd5ee639e4660ea5cb837cc19b84e
Lance <lance@Lances-MacBook-Pro.local>
2024-09-04 16:27:53 -0500

    worked on the merch tab

--- app/merch/page.jsx
+++ app/merch/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import merchcss from "./merch.module.css";
 import Image from "next/image";
-import tshirt from "/public/tshirt.jpeg";
+import tshirt from "/public/testproduct.webp";
 
 export default function merch() {
   return (
@@ -9,23 +9,23 @@ export default function merch() {
       <div className={merchcss.container}>
         <div className={merchcss.box}>
           <Image className={merchcss.image} src={tshirt} />
-          $60
+          <button className={merchcss.btn}>Add to cart</button> $40
         </div>
         <div className={merchcss.box}>
           <Image className={merchcss.image} src={tshirt} />
-          $60
+          <button className={merchcss.btn}>Add to cart</button> $25
         </div>
         <div className={merchcss.box}>
           <Image className={merchcss.image} src={tshirt} />
-          $60
+          <button className={merchcss.btn}>Add to cart</button> $75
         </div>
         <div className={merchcss.box}>
           <Image className={merchcss.image} src={tshirt} />
-          $60
+          <button className={merchcss.btn}>Add to cart</button> $60
         </div>
         <div className={merchcss.box}>
           <Image className={merchcss.image} src={tshirt} />
-          $60
+          <button className={merchcss.btn}>Add to cart</button> $10
         </div>
       </div>
     </main>
