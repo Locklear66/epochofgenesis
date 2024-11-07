8eca02c157b7c64437bbf88254aee0812521d407
Lance <lance@Lances-MacBook-Pro.local>
2024-11-07 17:38:56 -0600

    help

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,116 +1,37 @@
-"use client";
-import LoginCSS from "./login.module.css";
-import React from "react";
-import { auth } from "../firebase/init";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-} from "firebase/auth";
-import { useState, useEffect } from "react";
+'use client'
+import { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import {auth} from '@/app/Firebase/config'
+import logincss from './login.module.css'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-function login() {
-  const [user, setUser] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [signInEmail, setSignInEmail] = useState("");
-  const [signInPassword, setSignInPassword] = useState("");
+export default function account() {
+  const [email, setEmail] = useState('');
+  cosnt [passwoord, setPassword] = ('');
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setLoading(false);
-      console.log(user);
-      if (user) {
-        setUser(user);
-      }
-    });
-  }, []);
+  const [useCreateUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth)
 
-  function register() {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((user) => {
-        console.log(user);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+  const handleSignUp = async () => {
+    try{
+      const res = await createUserWithEmailAndPassword(email, password)
+      console.log({res})
+      setEmail('')
+      setPassword('')
+    } catch(e){
+      console.error(e)
+    }
+  };
 
-  function login() {
-    signInWithEmailAndPassword(auth, signInEmail, signInPassword)
-      .then(({ user }) => {
-        console.log(user);
-        setUser(user);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  function logout() {
-    signOut(auth);
-    setUser({});
-  }
   return (
-    <div>
-      <div className={LoginCSS.form__wrapper}>
-        <form className={LoginCSS.login__wrapper}>
-          <input
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            type="email"
-            className={LoginCSS.email__input}
-            placeholder="Enter email"
-            required
-          />
-          <input
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            type="password"
-            className={LoginCSS.password__input}
-            placeholder="Password"
-            required
-          />
-          <button onClick={register} className={LoginCSS.login__btn}>
-            Register
-          </button>
-        </form>
-        <form className={LoginCSS.login__wrapper}>
-          <input
-            value={signInEmail}
-            onChange={(event) => setSignInEmail(event.target.value)}
-            type="email"
-            className={LoginCSS.email__input}
-            placeholder="Enter email"
-            required
-          />
-          <input
-            value={signInPassword}
-            onChange={(event) => setSignInPassword(event.target.value)}
-            type="password"
-            className={LoginCSS.password__input}
-            placeholder="Password"
-            required
-          />
-          <button onClick={login} className={LoginCSS.login__btn}>
-            Login
-          </button>
-        </form>
+    <main className={logincss.main}>
+      <div className={logincss.wrapper}>
+       <div className={logincss.btn__wrapper}>
+         <input className={logincss.btn} onChange={(e) => setEmail(e.target.value)} type="text" autocomplete="off" name="text" class="input" placeholder="Username"></input>
+         <input className={logincss.btn} onChange={(e) => setPassword(e.target.value)} type="text" autocomplete="off" name="text" class="input" placeholder="Password"></input>
+         <button className={logincss.btn__login} onClick={handleSignUp}>Login</button>
+        </div>
       </div>
-      <div className={LoginCSS.logout__wrapper}>
-        <button
-          onClick={logout}
-          id={LoginCSS.logout__btn}
-          className={LoginCSS.login__btn}
-        >
-          Logout
-        </button>
-        {loading ? "loading..." : user.email}
-      </div>
-    </div>
-  );
+    </main>
+  )
 }
 
-export default login;
